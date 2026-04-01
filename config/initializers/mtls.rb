b5f4a231450a6419aa2281317ced0bfb35db34e4
End <contact@endoftimee.tech>
2026-04-01 10:01:56 -0700

    work?

--- config/initializers/mtls.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "../../lib/middleware/mtls_client_verifier"
-
-# Insert mTLS client certificate extraction middleware.
-# This must run before controllers so request.env["mtls.*"] is available.
-Rails.application.config.middleware.use MtlsClientVerifier
